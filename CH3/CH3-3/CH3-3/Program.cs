80b283d1932f81305ffe466a018628251c9ce1f0
ptcriuer <su7807@yahoo.com.tw>
2025-11-25 22:29:05 +0800

    新增CH3-3 & CH3-4

---
+++ CH3/CH3-3/CH3-3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("1-300的質數如下:");
+
+for (int i = 2; i < 300; i++)
+{
+    bool isPrime = true;
+    for (global::System.Int32 j = 2; j < i; j++)
+    {
+        if (i % j == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime)
+    {
+        Console.Write(i + " ");
+    }
+}
---
+++ CH3/CH3-4/CH3-4/Program.cs
@@ -0,0 +1,21 @@
+﻿string yes ="";
+int num;
+string symbol="";
+string str = "";
+do
+{
+    Console.WriteLine("請輸入欲顯示的符號:");
+    symbol = Console.ReadLine();
+    Console.WriteLine("請輸入重複地次數:");
+    num = int.Parse(Console.ReadLine());
+    for (global::System.Int32 i = 1; i <= num; i++)
+    {
+        str += symbol;
+    }
+    Console.WriteLine(str);
+    str = "";
+    Console.WriteLine("是否繼續(按 y or Y 繼續):");
+    yes = Console.ReadLine();
+    
+}
+while (yes == "y" || yes =="Y");
