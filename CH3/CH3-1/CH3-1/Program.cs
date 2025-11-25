7c68401610e9ac761fbeab0a84b634a7598aae8b
evan.su <su7807@yahoo.com.tw>
2025-11-25 22:04:42 +0800

    新增CH3練習

---
+++ CH3/CH3-1/CH3-1/Program.cs
@@ -0,0 +1,32 @@
+﻿int first;
+int second;
+string op;
+int output = 0;
+Console.Write("\n1.請輸入第一個運算元:");
+first = int.Parse(Console.ReadLine());
+Console.Write("\n2.請輸入第二個運算元:");
+second = int.Parse(Console.ReadLine());
+Console.Write("\n2.請輸入運算子:加(+) 減(-) 乘(*) 除(/):");
+op = Console.ReadLine();
+
+if(op == "*")
+{
+    output = first * second;
+}
+else if (op == "+")
+{
+    output = first + second;
+}
+else if(op == "-")
+{
+    output = first - second;
+}
+else if(op == "/")
+{
+    output = first / second;
+}
+else
+{
+    Console.WriteLine("您可能輸入有誤!");
+}
+Console.WriteLine($"運算結果: {first} {op} {second} = {output}");
---
+++ CH3/CH3-2/CH3-2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("\n1.聯合 信用卡");
+Console.WriteLine("\n2.JCB 信用卡");
+Console.WriteLine("\n3.VISA 信用卡");
+Console.WriteLine("\n4.MASTER 信用卡");
+int input;
+Console.Write("\n請輸入欲使用的信用卡種類:");
+input = int.Parse(Console.ReadLine());
+
+switch (input)
+{
+    case 03:
+        Console.WriteLine("\n您是持 聯合 信用卡付費");
+        break;
+    case 35:
+        Console.WriteLine("\n您是持 JCB 信用卡付費");
+        break;
+    case 45:
+        Console.WriteLine("\n您是持 VISA 信用卡付費");
+        break;
+    case 54:
+        Console.WriteLine("\n您是持 MASTER 信用卡付費");
+        break;
+}
